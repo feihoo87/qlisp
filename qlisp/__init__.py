b97910750e2b258a4fc4b600627db06fcb03cc96
feihoo87 <xfh1987@gmail.com>
2024-01-16 12:52:25 +0800

    update namespace

--- qlisp/__init__.py
+++ qlisp/__init__.py
@@ -7,6 +7,13 @@ from .matricies import (CR, CX, CZ, SWAP, BellPhiM, BellPhiP, BellPsiM,
 from .simple import applySeq, regesterGateMatrix, seq2mat
 
 try:
-    from qlispc import *
+    from qlispc import (COMMAND, FREE, NOTSET, PUSH, READ, SYNC, TRIG, WRITE,
+                        ABCCompileConfigMixin, ADChannel, Architecture,
+                        AWGChannel, Capture, CommandList, Config, ConfigProxy,
+                        DataMap, GateConfig, Library, MultADChannel,
+                        MultAWGChannel, Program, ProgramFrame, QLispCode,
+                        RawData, Result, Signal, add_VZ_rule, compile,
+                        get_arch, libraries, mapping_qubits, register_arch,
+                        stdlib)
 except ImportError:
     pass
