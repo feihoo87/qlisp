fd3f41654499bc5a67269ec335b20a72bb6c6e4c
feihoo87 <xfh1987@gmail.com>
2025-02-23 12:55:11 +0800

    update RB

--- qlisp/circuits/rb.py
+++ qlisp/circuits/rb.py
@@ -1,106 +1,226 @@
 import random
+from typing import Sequence
 
 import numpy as np
-from cycles.clifford import cliffordOrder
 
-from ..clifford.utils import twoQubitCliffordSequence
-from ..simple import seq2mat
-from ._rb.clifford import inv, mat2index, mul
-from .utils import mapping_qubits
+from qlisp.clifford import CliffordGroup, Cycles
+from qlisp.clifford.utils import (one_qubit_clifford_seq,
+                                  two_qubit_clifford_circuit)
 
-_index2seq = [twoQubitCliffordSequence(i) for i in range(cliffordOrder(2))]
+two_qubit_clifford_group = CliffordGroup(2)
+two_qubit_clifford_elements = {}
 
-
-def twoQubitGate(gates):
-    return {
-        ('CZ', 'CZ'): ('CZ', (0, 1)),
-        ('C', 'Z'): ('CZ', (0, 1)),
-        ('Z', 'C'): ('CZ', (0, 1)),
-        ('CX', 'CX'): ('Cnot', (0, 1)),
-        ('XC', 'XC'): ('Cnot', (1, 0)),
-        ('CR', 'CR'): ('CR', (0, 1)),
-        ('RC', 'RC'): ('CR', (1, 0)),
-        ('C', 'X'): ('Cnot', (0, 1)),
-        ('X', 'C'): ('Cnot', (1, 0)),
-        ('C', 'R'): ('CR', (0, 1)),
-        ('R', 'C'): ('CR', (1, 0)),
-        ('iSWAP', 'iSWAP'): ('iSWAP', (0, 1)),
-        ('SWAP', 'SWAP'): ('SWAP', (0, 1)),
-        ('SQiSWAP', 'SQiSWAP'): ('SQiSWAP', (0, 1)),
-    }[gates]
+groups = {}
 
 
-def seq2qlisp(seq, qubits):
-    if len(seq) > 2:
-        raise ValueError("Only support 1 or 2 bits.")
-    if len(seq) != len(qubits):
-        raise ValueError("seq size and qubit num mismatched.")
-
-    qlisp = []
-    for gates in zip(*seq):
-        try:
-            qlisp.append(twoQubitGate(gates))
-        except:
-            for gate, i in zip(gates, qubits):
-                qlisp.append((gate, i))
-    return qlisp
-
-
-def circuit_to_index(circuit: list) -> int:
-    if not circuit:
-        return 0
-    mat = seq2mat(circuit)
-    if mat.shape[0] == 2:
-        mat = np.kron(np.eye(2), mat)
-    return mat2index(mat)
-
-
-def index_to_circuit(index: int, qubits=(0, ), base=None, rng=None) -> list:
-    if len(qubits) > 2:
-        raise ValueError('Only support 1 or 2 qubits')
-    if rng is None:
-        rng = random.Random()
-    if base is None:
-        base = _index2seq
-    seq = rng.choice(base[index])
-    if len(qubits) == 1:
-        seq = (seq[1], )
-    return seq2qlisp(seq, range(len(qubits)))
-
-
-def generateRBCircuit(qubits, cycle, seed=None, interleaves=[], base=None):
-    """Generate a random Clifford RB circuit.
+def one_qubit_rb(cycles: int,
+                 interleaved: list = [],
+                 seed: int | None = None) -> list:
+    """
+    Generate a random one qubit RB circuit.
 
     Args:
-        qubits (list): The qubits to use.
-        cycle (int): The cycles of clifford sequence.
+        cycles (int): The cycles of sequence.
+        interleaved (list): The interleaved to use.
         seed (int): The seed for the random number generator.
-        interleaves (list): The interleaves to use.
-        base (list): The basic two-qubit Clifford sequence.
-
-    Returns:
-        list: The RB circuit.
     """
-    if isinstance(qubits, (str, int)):
-        qubits = {0: qubits}
+    from qlisp import seq2mat
+    from qlisp.matricies import Unitary2Angles
+
+    rng = random.Random()
+    if seed is not None:
+        rng.seed(int(seed))
+
+    circ = []
+    for i in range(cycles):
+        gate = rng.choice(one_qubit_clifford_seq)
+        circ.append((gate, 0))
+        for gate in interleaved:
+            circ.append((gate, 0))
+    U = seq2mat(circ)
+    theta, phi, lambda_, *_ = Unitary2Angles(np.linalg.inv(U))
+    circ.append((('u3', theta, phi, lambda_), 0))
+
+    return circ
+
+
+def two_qubit_rb(cycles: int,
+                 interleaved: list = [],
+                 allowed_two_qubit_gates: Sequence[str] = ('CZ', 'CX', 'iSWAP',
+                                                           'SWAP'),
+                 seed: int | None = None) -> list:
+    """
+    Generate a random two qubit RB circuit.
+
+    Args:
+        cycles (int): The cycles of sequence.
+        interleaved (list): The interleaved to use.
+        allowed_two_qubit_gates (list): The allowed two qubit gates.
+        seed (int): The seed for the random number generator.
+    """
+
+    if len(two_qubit_clifford_elements) == 0:
+        for i in range(11520):
+            circ = two_qubit_clifford_circuit(i)
+            perm = two_qubit_clifford_group.circuit_to_permutation(circ)
+            two_qubit_clifford_elements[perm] = i
+            two_qubit_clifford_elements[i] = perm
+
+    rng = random.Random()
+    if seed is not None:
+        rng.seed(int(seed))
+
+    circ = []
+    perm = Cycles()
+    interleaved_perm = two_qubit_clifford_group.circuit_to_permutation(
+        interleaved)
+    for i in range(cycles):
+        n = rng.choice(range(two_qubit_clifford_group.order()))
+        circ.extend(two_qubit_clifford_circuit(n, allowed_two_qubit_gates))
+        circ.extend(interleaved)
+        perm = perm * two_qubit_clifford_elements[n] * interleaved_perm
+    inv = two_qubit_clifford_elements[perm.inv()]
+    circ.extend(two_qubit_clifford_circuit(inv, allowed_two_qubit_gates))
+
+    return circ
+
+
+def n_qubit_rb(n: int,
+               cycles: int,
+               interleaved: list = [],
+               two_qubit_gate='CZ',
+               seed=None):
+    """
+    Generate a random n qubit RB circuit.
+
+    Args:
+        n (int): The number of qubits.
+        cycles (int): The cycles of sequence.
+        two_qubit_gate (str): The two qubit gate to use.
+        interleaved (list): The interleaved to use.
+        seed (int): The seed for the random number generator.
+    """
+    if (n, two_qubit_gate) not in groups:
+        groups[(n,
+                two_qubit_gate)] = CliffordGroup(n,
+                                                 two_qubit_gate=two_qubit_gate)
+    group = groups[(n, two_qubit_gate)]
+
+    if seed is None:
+        rng = random.Random()
     else:
-        qubits = {i: q for i, q in enumerate(qubits)}
+        rng = random.Random(seed)
 
-    MAX = cliffordOrder(len(qubits))
+    interleaved_perm = group.circuit_to_permutation(interleaved)
+    circ = []
+    perm = Cycles()
+    for i in range(cycles):
+        p = group.random(rng=rng)
+        circ.extend(group.permutation_to_circuit(p))
+        circ.extend(interleaved)
+        perm = perm * p * interleaved_perm
+    circ.extend(group.permutation_to_circuit(perm.inv()))
+    return circ
 
-    interleaves_index = circuit_to_index(interleaves)
 
+def mapping_qubits(circuit, mapping):
+    """
+    Remap qubits in a circuit.
+
+    Args:
+        circuit: [(gate, *target), ...]
+        mapping: {old: new}
+    """
     ret = []
-    index = 0
-    rng = random.Random(seed)
+    for gate, *target in circuit:
+        ret.append((gate, *tuple(mapping.get(i, i) for i in target)))
+    return ret
 
-    for _ in range(cycle):
-        i = rng.randrange(MAX)
-        index = mul(i, index)
-        ret.extend(index_to_circuit(i, qubits, base, rng))
-        index = mul(interleaves_index, index)
-        ret.extend(interleaves)
 
-    ret.extend(index_to_circuit(inv(index), qubits, base, rng))
+def circuit_one_qubit_rb(qubits,
+                         cycles: int,
+                         interleaved: list | None = None,
+                         seeds=None):
+    if interleaved is None:
+        interleaved = []
 
-    return mapping_qubits(ret, qubits)
+    circs = []
+    if isinstance(seeds, (int, np.int_)):
+        seeds = [seeds] * len(qubits)
+    for q, seed in zip(qubits, seeds):
+        circs.append(
+            mapping_qubits(one_qubit_rb(cycles, interleaved, seed), {0: q}))
+
+    circ = []
+
+    for segs in zip(*circs):
+        circ.extend(list(segs))
+        circ.append(('Barrier', tuple(qubits)))
+
+    return circ
+
+
+def circuit_two_qubit_rb(pairs,
+                         cycles: int,
+                         interleaved: list | None = None,
+                         allowed_two_qubit_gates: Sequence[str] = ('CZ', ),
+                         seeds=None):
+    if interleaved is None:
+        interleaved = []
+
+    circs = []
+    if isinstance(seeds, (int, np.int_)):
+        seeds = [seeds] * len(pairs)
+    for pair, seed in zip(pairs, seeds):
+        circs.append(
+            mapping_qubits(
+                two_qubit_rb(cycles, interleaved, allowed_two_qubit_gates,
+                             seed), {
+                                 0: pair[0],
+                                 1: pair[1]
+                             }))
+
+    return circs
+
+
+def circuit_rb(qubit_groups,
+               cycles: int,
+               interleaved: list | None = None,
+               two_qubit_gate='CZ',
+               seeds=None):
+    if interleaved is None:
+        interleaved = []
+    interleaved_qubits = set()
+    for gate, *qubits in interleaved:
+        interleaved_qubits.update(*qubits)
+
+    circs = []
+    if isinstance(seeds, (int, np.int_)) or seeds is None:
+        seeds = [seeds] * len(qubit_groups)
+    for qubits, seed in zip(qubit_groups, seeds):
+        if len(qubits) == 1:
+            if interleaved_qubits.issubset({
+                    0,
+            }):
+                circ = one_qubit_rb(cycles, interleaved, seed)
+            else:
+                raise ValueError(
+                    "Interleaved qubits must be 0 for one qubit RB.")
+        elif len(qubits) == 2:
+            if interleaved_qubits.issubset({0, 1}):
+                circ = two_qubit_rb(cycles, interleaved, (two_qubit_gate, ),
+                                    seed)
+            else:
+                raise ValueError(
+                    "Interleaved qubits must be 0, 1 for two qubit RB.")
+        else:
+            if interleaved_qubits.issubset(set(range(len(qubits)))):
+                circ = n_qubit_rb(len(qubits), cycles, interleaved,
+                                  two_qubit_gate, seed)
+            else:
+                raise ValueError(
+                    f"Interleaved qubits must be {set(range(len(qubits)))} for {len(qubits)} qubit RB."
+                )
+        mapping = {i: q for i, q in enumerate(qubits)}
+        circs.append(mapping_qubits(circ, mapping))
+    return circs
