7ed66051859994e61fb60e892af75eeae8422e76
feihoo87 <xfh1987@gmail.com>
2025-03-05 15:44:41 +0800

    update xeb

--- qlisp/circuits/xeb.py
+++ qlisp/circuits/xeb.py
@@ -49,6 +49,47 @@ def Fxeb(Pm_lst: Iterable[np.array],
     return (np.mean(Si) - np.mean(Sm)) / (np.mean(Si) - np.mean(Se))
 
 
+def Fxeb_linear(measured: Iterable[np.array], ideal: Iterable[np.array]):
+    """XEB Fidelity
+
+    Linear cross entropy between two probability distributions p and q is
+    defined as:
+        S(p, q) = SUM [ p * (1 - D * q) ]
+    where D is the Hilbert space dimension.
+
+    The fidelity is defined as normalized linear cross entropy between the
+    ideal and measured distributions, with values between 0 and 1. In case
+    of perfect fidelity, the value is 1, and in case of perfect mixing, the
+    value is 0.
+
+    The fidelity is calculated as:
+            F = (S(p, q) - S(p, i)) / (S(p, p) - S(p, i))
+    where p is the ideal distribution, q is the measured distribution, and
+    i is the uniform distribution. In the case of the linear cross entropy,
+    S(p, i) always equals 0, so the formula simplifies to:
+            F = S(p, q) / S(p, p)
+
+    The fidelity decays exponentially with the cycles, fitting the data with
+            F = p ** cycle
+    where p is the fidelity per cycle, it can be used to estimate the Pauli
+    error rate:
+            ep = (1 - p) / (1 - 1/D**2)
+    Pauli error rate is the probability of a total Pauli error per cycle.
+    The totalvPauli error is the sum of the all kinds of Pauli errors, and
+    each kind of Pauli error assumes the same probability.
+
+    Ref:
+        https://doi.org/10.1038/s41586-019-1666-5
+    """
+    measured = np.asarray(measured)
+    ideal = np.asarray(ideal)
+    D = ideal.shape[-1]
+    b = 1 / D**2
+    F = (np.mean(ideal * measured, axis=(-1, -2)) -
+         b) / (np.mean(ideal**2, axis=(-1, -2)) - b)
+    return F
+
+
 def specklePurity(Pm_lst: Iterable[np.array], D: int = None) -> float:
     """Speckle Purity
 
@@ -129,4 +170,3 @@ def generateXEBCircuit(qubits: Union[int, str, tuple],
             ret.append((rng.choice(base), q))
 
     return ret
-
